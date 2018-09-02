04e7649397b8f2fd71b5123d512245174bda03d8
AdamWier <38018787+AdamWier@users.noreply.github.com>
2018-09-02 19:27:21 +0200

    hello

--- cash.js
+++ cash.js
@@ -66,3 +66,4 @@ function checkCashRegister(price, cash, cid) {
 
 
 checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
+console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
